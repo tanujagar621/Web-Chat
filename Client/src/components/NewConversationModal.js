0c2314dd9e5f27c42c046e168c78f56f903d438f
tanujagar621 <tanujagarwal621@gmail.com>
2021-06-15 19:12:45 +0530

    added login and dashboard page conversations remaining

--- Client/src/components/NewConversationModal.js
+++ Client/src/components/NewConversationModal.js
@@ -1,22 +1,10 @@
 import React from "react";
 import { Modal, Form, Button } from "react-bootstrap";
-import {contacts} from '../contexts/ContactsProvider'
+import { contacts } from "../contexts/ContactsProvider";
 export default function NewConversationModal(props) {
   return (
     <>
       <Modal.Header closeButton>Create Conversation</Modal.Header>
-      <Modal.Body>
-        <Form onSubmit={handleSubmit}>
-          {contacts.map(contact => (
-            
-          ))}
-          {/* <Form.Group className="my-2">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" ref={nameRef} required></Form.Control>
-          </Form.Group> */}
-          <Button type="submit">Create</Button>
-        </Form>
-      </Modal.Body>
     </>
   );
 }
